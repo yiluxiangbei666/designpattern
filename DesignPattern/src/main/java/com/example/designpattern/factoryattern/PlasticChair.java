10d9272bbaf301319ae7fa12ec869ffad202efc5
fgz <1182764581@qq.com>
2021-07-25 20:14:19 +0800

    dasda

--- DesignPattern/src/main/java/com/example/designpattern/factoryattern/PlasticChair.java
+++ DesignPattern/src/main/java/com/example/designpattern/factoryattern/PlasticChair.java
@@ -3,13 +3,6 @@ package com.example.designpattern.factoryattern;
 public class PlasticChair implements Chair{
     @Override
     public String getType() {
-        System.out.println("dsaasda");
-        System.out.println("dsaasda");
-        System.out.println("dsaasda1");
-        System.out.println("dsaasda122");
-        System.out.println("dsaasda122");
-        System.out.println("dsaasda12");
-        System.out.println("dsaasda12");
         return "塑料椅";
     }
 }
--- DesignPattern/src/main/java/com/example/designpattern/factoryattern/PlasticDesk.java
+++ DesignPattern/src/main/java/com/example/designpattern/factoryattern/PlasticDesk.java
@@ -3,8 +3,6 @@ package com.example.designpattern.factoryattern;
 public class PlasticDesk implements Desk{
     @Override
     public String getType() {
-        System.out.println("dsadasd");
-        System.out.println("dsadasd");
         return "塑料桌";
     }
 }
