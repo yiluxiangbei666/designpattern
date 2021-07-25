fc083eb661ed70c002f67f65672078ab93c41481
fgz <1182764581@qq.com>
2021-07-25 19:36:51 +0800

    adad

--- DesignPattern/src/main/java/com/example/designpattern/factoryattern/PlasticChair.java
+++ DesignPattern/src/main/java/com/example/designpattern/factoryattern/PlasticChair.java
@@ -3,7 +3,7 @@ package com.example.designpattern.factoryattern;
 public class PlasticChair implements Chair{
     @Override
     public String getType() {
-        System.out.println("dsaasda1");
+        System.out.println("dsaasda12");
         return "塑料椅";
     }
 }
